bda10230a76bd14e6b306fa9167909ce7c8b08d8
zhangjessey <zhangjessey@gmail.com>
2018-11-21 15:54:27 +0800

    add annotation for ChannelInboundHandler

--- transport/src/main/java/io/netty/channel/ChannelInboundHandler.java
+++ transport/src/main/java/io/netty/channel/ChannelInboundHandler.java
@@ -19,20 +19,33 @@ package io.netty.channel;
  * {@link ChannelHandler} which adds callbacks for state changes. This allows the user
  * to hook in to state changes easily.
  */
+
+/**
+ * ChannelHandler可以为状态变更添加回调。允许用户在状态变化前hook
+ */
 public interface ChannelInboundHandler extends ChannelHandler {
 
     /**
      * The {@link Channel} of the {@link ChannelHandlerContext} was registered with its {@link EventLoop}
+     */
+    /**
+     * ChannelHandlerContext的Channel被注册到它的EventLoop
      */
     void channelRegistered(ChannelHandlerContext ctx) throws Exception;
 
     /**
      * The {@link Channel} of the {@link ChannelHandlerContext} was unregistered from its {@link EventLoop}
      */
+    /**
+     * ChannelHandlerContext的Channel被解除注册从它的EventLoop
+     */
     void channelUnregistered(ChannelHandlerContext ctx) throws Exception;
 
     /**
      * The {@link Channel} of the {@link ChannelHandlerContext} is now active
+     */
+    /**
+     * ChannelHandlerContext的Channel被激活
      */
     void channelActive(ChannelHandlerContext ctx) throws Exception;
 
@@ -40,10 +53,16 @@ public interface ChannelInboundHandler extends ChannelHandler {
      * The {@link Channel} of the {@link ChannelHandlerContext} was registered is now inactive and reached its
      * end of lifetime.
      */
+    /**
+     * ChannelHandlerContext的Channel被解除激活并且到达它的生命周期终点
+     */
     void channelInactive(ChannelHandlerContext ctx) throws Exception;
 
     /**
      * Invoked when the current {@link Channel} has read a message from the peer.
+     */
+    /**
+     * 当前Channel读到消息时被调用
      */
     void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception;
 
@@ -53,10 +72,17 @@ public interface ChannelInboundHandler extends ChannelHandler {
      * attempt to read an inbound data from the current {@link Channel} will be made until
      * {@link ChannelHandlerContext#read()} is called.
      */
+    /**
+     * 当前读操作读取的最后一条消息已经被channelRead方法消费时调用。如果ChannelOption是off,不再尝试读取入数据从当前Channel
+     * 直到ChannelHandlerContext的read方法被调用
+     */
     void channelReadComplete(ChannelHandlerContext ctx) throws Exception;
 
     /**
      * Gets called if an user event was triggered.
+     */
+    /**
+     * 当用户事件被触发时调用
      */
     void userEventTriggered(ChannelHandlerContext ctx, Object evt) throws Exception;
 
@@ -64,10 +90,16 @@ public interface ChannelInboundHandler extends ChannelHandler {
      * Gets called once the writable state of a {@link Channel} changed. You can check the state with
      * {@link Channel#isWritable()}.
      */
+    /**
+     * Channel的可写状态被改变时调用。你可以通过Channel的isWritable方法检查状态。
+     */
     void channelWritabilityChanged(ChannelHandlerContext ctx) throws Exception;
 
     /**
      * Gets called if a {@link Throwable} was thrown.
+     */
+    /**
+     * 抛出异常时被调用。
      */
     @Override
     @SuppressWarnings("deprecation")
