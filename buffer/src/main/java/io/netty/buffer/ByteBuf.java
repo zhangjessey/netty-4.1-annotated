61ce219734371cfeeb06e26213601ef0f25ca281
zhangjessey <zhangjessey@gmail.com>
2018-11-26 22:37:02 +0800

    add annotation for ByteBuf

--- buffer/src/main/java/io/netty/buffer/ByteBuf.java
+++ buffer/src/main/java/io/netty/buffer/ByteBuf.java
@@ -28,6 +28,221 @@ import java.nio.channels.GatheringByteChannel;
 import java.nio.channels.ScatteringByteChannel;
 import java.nio.charset.Charset;
 import java.nio.charset.UnsupportedCharsetException;
+/**
+ * 一个零个或者更多字节的可以随机并且顺序访问的序列。
+ * 这个接口为一个或者多个原始类型数组以及ByteBuffer提供一种抽象视图。
+ *
+ * buffer的创建
+ *
+ * 推荐使用Unpooled类里的工具方法而不是调用一个独立的构造方法的实现来创建一个新的buffer。
+ *
+ * 随机访问索引
+ *
+ * 就像一个普通的原始类型数组，ByteBuf使用
+ *<a href="http://en.wikipedia.org/wiki/Zero-based_numbering">从零开始的索引</a>.
+ * 这意味着第一个字节的索引总是0，最后一个字节的索引总是capacity - 1。比如说，要迭代
+ * 一个buffer的所有字节，你可以做下面这些事，不管它的内部实现。
+ *  *
+ *  * <pre>
+ *  * {@link ByteBuf} buffer = ...;
+ *  * for (int i = 0; i &lt; buffer.capacity(); i ++) {
+ *  *     byte b = buffer.getByte(i);
+ *  *     System.out.println((char) b);
+ *  * }
+ *  * </pre>
+ *
+ *  顺序访问索引
+ *  ByteBuf提供两个指针变量来支持顺序读和写操作 - readerIndex()writerIndex()分别用于读操作和
+ *  写操作。下图展示一个buffer怎样被两个指针分成三个部分：
+ *
+ * <pre>
+ *      +-------------------+------------------+------------------+
+ *      | discardable bytes |  readable bytes  |  writable bytes  |
+ *      |                   |     (CONTENT)    |                  |
+ *      +-------------------+------------------+------------------+
+ *      |                   |                  |                  |
+ *      0      <=      readerIndex   <=   writerIndex    <=    capacity
+ * </pre>
+ *
+ * 可读取的字节（实际内容）
+ *
+ * 这一段是实际数据被存储的地方。任何以read或skip开头的名字的操作都会获取或者跳过
+ * readerIndex的数据，并且根据可读数据的数量增加它。如果读操作的参数也是ByteBuf，
+ * 并且没有指定目的地索引，特定buffer的writerIndex也会同步被增加。
+ *
+ * 如果没有足够的content留下，IndexOutOfBoundsException会被抛出。刚刚被分配，
+ * 包裹，复制的buffer的readerIndex的默认值是0。
+ *
+ * <pre>
+ * // Iterates the readable bytes of a buffer.
+ * {@link ByteBuf} buffer = ...;
+ * while (buffer.isReadable()) {
+ *     System.out.println(buffer.readByte());
+ * }
+ * </pre>
+ *
+ * 可写的字节
+ *
+ * 这一段是需要被填充的未定义空间。
+ * TODO
+ *
+ * This segment is a undefined space which needs to be filled.  Any operation
+ * whose name starts with {@code write} will write the data at the current
+ * {@link #writerIndex() writerIndex} and increase it by the number of written
+ * bytes.  If the argument of the write operation is also a {@link ByteBuf},
+ * and no source index is specified, the specified buffer's
+ * {@link #readerIndex() readerIndex} is increased together.
+ * <p>
+ * If there's not enough writable bytes left, {@link IndexOutOfBoundsException}
+ * is raised.  The default value of newly allocated buffer's
+ * {@link #writerIndex() writerIndex} is {@code 0}.  The default value of
+ * wrapped or copied buffer's {@link #writerIndex() writerIndex} is the
+ * {@link #capacity() capacity} of the buffer.
+ *
+ * <pre>
+ * // Fills the writable bytes of a buffer with random integers.
+ * {@link ByteBuf} buffer = ...;
+ * while (buffer.maxWritableBytes() >= 4) {
+ *     buffer.writeInt(random.nextInt());
+ * }
+ * </pre>
+ *
+ * <h4>Discardable bytes</h4>
+ *
+ * This segment contains the bytes which were read already by a read operation.
+ * Initially, the size of this segment is {@code 0}, but its size increases up
+ * to the {@link #writerIndex() writerIndex} as read operations are executed.
+ * The read bytes can be discarded by calling {@link #discardReadBytes()} to
+ * reclaim unused area as depicted by the following diagram:
+ *
+ * <pre>
+ *  BEFORE discardReadBytes()
+ *
+ *      +-------------------+------------------+------------------+
+ *      | discardable bytes |  readable bytes  |  writable bytes  |
+ *      +-------------------+------------------+------------------+
+ *      |                   |                  |                  |
+ *      0      <=      readerIndex   <=   writerIndex    <=    capacity
+ *
+ *
+ *  AFTER discardReadBytes()
+ *
+ *      +------------------+--------------------------------------+
+ *      |  readable bytes  |    writable bytes (got more space)   |
+ *      +------------------+--------------------------------------+
+ *      |                  |                                      |
+ * readerIndex (0) <= writerIndex (decreased)        <=        capacity
+ * </pre>
+ *
+ * Please note that there is no guarantee about the content of writable bytes
+ * after calling {@link #discardReadBytes()}.  The writable bytes will not be
+ * moved in most cases and could even be filled with completely different data
+ * depending on the underlying buffer implementation.
+ *
+ * <h4>Clearing the buffer indexes</h4>
+ *
+ * You can set both {@link #readerIndex() readerIndex} and
+ * {@link #writerIndex() writerIndex} to {@code 0} by calling {@link #clear()}.
+ * It does not clear the buffer content (e.g. filling with {@code 0}) but just
+ * clears the two pointers.  Please also note that the semantic of this
+ * operation is different from {@link ByteBuffer#clear()}.
+ *
+ * <pre>
+ *  BEFORE clear()
+ *
+ *      +-------------------+------------------+------------------+
+ *      | discardable bytes |  readable bytes  |  writable bytes  |
+ *      +-------------------+------------------+------------------+
+ *      |                   |                  |                  |
+ *      0      <=      readerIndex   <=   writerIndex    <=    capacity
+ *
+ *
+ *  AFTER clear()
+ *
+ *      +---------------------------------------------------------+
+ *      |             writable bytes (got more space)             |
+ *      +---------------------------------------------------------+
+ *      |                                                         |
+ *      0 = readerIndex = writerIndex            <=            capacity
+ * </pre>
+ *
+ * <h3>Search operations</h3>
+ *
+ * For simple single-byte searches, use {@link #indexOf(int, int, byte)} and {@link #bytesBefore(int, int, byte)}.
+ * {@link #bytesBefore(byte)} is especially useful when you deal with a {@code NUL}-terminated string.
+ * For complicated searches, use {@link #forEachByte(int, int, ByteProcessor)} with a {@link ByteProcessor}
+ * implementation.
+ *
+ * <h3>Mark and reset</h3>
+ *
+ * There are two marker indexes in every buffer. One is for storing
+ * {@link #readerIndex() readerIndex} and the other is for storing
+ * {@link #writerIndex() writerIndex}.  You can always reposition one of the
+ * two indexes by calling a reset method.  It works in a similar fashion to
+ * the mark and reset methods in {@link InputStream} except that there's no
+ * {@code readlimit}.
+ *
+ * <h3>Derived buffers</h3>
+ *
+ * You can create a view of an existing buffer by calling one of the following methods:
+ * <ul>
+ *   <li>{@link #duplicate()}</li>
+ *   <li>{@link #slice()}</li>
+ *   <li>{@link #slice(int, int)}</li>
+ *   <li>{@link #readSlice(int)}</li>
+ *   <li>{@link #retainedDuplicate()}</li>
+ *   <li>{@link #retainedSlice()}</li>
+ *   <li>{@link #retainedSlice(int, int)}</li>
+ *   <li>{@link #readRetainedSlice(int)}</li>
+ * </ul>
+ * A derived buffer will have an independent {@link #readerIndex() readerIndex},
+ * {@link #writerIndex() writerIndex} and marker indexes, while it shares
+ * other internal data representation, just like a NIO buffer does.
+ * <p>
+ * In case a completely fresh copy of an existing buffer is required, please
+ * call {@link #copy()} method instead.
+ *
+ * <h4>Non-retained and retained derived buffers</h4>
+ *
+ * Note that the {@link #duplicate()}, {@link #slice()}, {@link #slice(int, int)} and {@link #readSlice(int)} does NOT
+ * call {@link #retain()} on the returned derived buffer, and thus its reference count will NOT be increased. If you
+ * need to create a derived buffer with increased reference count, consider using {@link #retainedDuplicate()},
+ * {@link #retainedSlice()}, {@link #retainedSlice(int, int)} and {@link #readRetainedSlice(int)} which may return
+ * a buffer implementation that produces less garbage.
+ *
+ * <h3>Conversion to existing JDK types</h3>
+ *
+ * <h4>Byte array</h4>
+ *
+ * If a {@link ByteBuf} is backed by a byte array (i.e. {@code byte[]}),
+ * you can access it directly via the {@link #array()} method.  To determine
+ * if a buffer is backed by a byte array, {@link #hasArray()} should be used.
+ *
+ * <h4>NIO Buffers</h4>
+ *
+ * If a {@link ByteBuf} can be converted into an NIO {@link ByteBuffer} which shares its
+ * content (i.e. view buffer), you can get it via the {@link #nioBuffer()} method.  To determine
+ * if a buffer can be converted into an NIO buffer, use {@link #nioBufferCount()}.
+ *
+ * <h4>Strings</h4>
+ *
+ * Various {@link #toString(Charset)} methods convert a {@link ByteBuf}
+ * into a {@link String}.  Please note that {@link #toString()} is not a
+ * conversion method.
+ *
+ * <h4>I/O Streams</h4>
+ *
+ * Please refer to {@link ByteBufInputStream} and
+ * {@link ByteBufOutputStream}.
+ */
+
+
+
+
+
+
+
+
 
 /**
  * A random and sequential accessible sequence of zero or more bytes (octets).
